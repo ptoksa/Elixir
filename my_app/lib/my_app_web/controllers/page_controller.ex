f3207db3ed5cfec7a0aa9a2f17199e218a33dc2e
poksa <petri.oksa@gmail.com>
2024-04-29 16:10:22 +0300

    Add directory changes

--- my_app/lib/my_app_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule MyAppWeb.PageController do
-  use MyAppWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
